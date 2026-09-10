266dfbd851e9a7c8d7d346b712db10d61dbfb348
Mohd Rizwan <mr659612@gmail.com>
2026-09-10 18:19:19 +0530

    Trap rain water brute force

---
+++ Arrays_HAshing/TapRainWater.java
@@ -0,0 +1,32 @@
+public class TapRainWater {
+    public static int trap(int[] height) {
+        
+        int collect = 0;
+        for(int i=1; i<height.length-1;i++)
+        {
+            int leftMax = height[i-1];
+            for(int j=i-1; j>=0; j--)
+            {
+                leftMax = Math.max(leftMax, height[j]);
+            }
+            
+       
+            int rightMax = height[i+1];
+            for(int j=i+1; j< height.length; j++)
+            {
+                rightMax = Math.max(rightMax, height[j]);
+            }
+            
+        int water = Math.max(0,Math.min(leftMax, rightMax) - height[i]);
+        collect += water;
+        }
+
+        return collect;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = {0,2,0,3,1,0,1,3,2,1};
+        int result = trap(heights);
+        System.out.print(result);
+    }
+}
---
+++ Arrays_HAshing/WaterContainerOptimal.java
@@ -0,0 +1,25 @@
+public class WaterContainerOptimal {
+    public static int maxArea(int[] heights)
+    {
+        int left = 0;
+        int right = heights.length-1;
+        int max = 0;
+        while(left < right)
+        {
+            int area = (right-left) * Math.min(heights[left], heights[right]); 
+            max = Math.max(max, area);
+
+            if(heights[left] < heights[right])
+                left++;
+            else
+                right--;
+        }
+
+        return max;
+    }
+    public static void main(String[] args){
+        int[] heights = {2,2,2};
+        int result = maxArea(heights);
+        System.out.println(result);
+    }
+}
